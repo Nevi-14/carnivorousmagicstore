518ab5fe18a3739b980684e685ad1a5c0149d31a
Nelson Morera <workemailnelson@gmail.com>
2022-03-26 21:08:43 -0600

    20220326v1.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,10 +13,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/image-modal/image-modal.module').then( m => m.ImageModalPageModule)
   },
   {
-    path: 'detalle-envio',
-    loadChildren: () => import('./pages/detalle-envio/detalle-envio.module').then( m => m.DetalleEnvioPageModule)
-  },
-  {
     path: 'checkout-intructions',
     loadChildren: () => import('./pages/checkout-intructions/checkout-intructions.module').then( m => m.CheckoutIntructionsPageModule)
   },
--- src/app/pages/check-out/check-out.page.ts
+++ src/app/pages/check-out/check-out.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import * as  mapboxgl from 'mapbox-gl';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import { DetalleEnvioPage } from '../detalle-envio/detalle-envio.page';
 import { ModalController } from '@ionic/angular';
 import { CheckoutIntructionsPage } from '../checkout-intructions/checkout-intructions.page';
 import { DeliveryDetailsPage } from '../delivery-details/delivery-details.page';
@@ -20,6 +19,8 @@ export class CheckOutPage implements OnInit, AfterViewInit {
   stepOne = true;
   stepTwo= false;
   stepThree= false;
+  coordinates = [];
+  features = [];
   constructor(
     public modalCtrl: ModalController
   ) { }
@@ -58,6 +59,14 @@ async deliveryDetails() {
   });
   return await modal.present();
 }
+irMarcador(item) {
+  if (item) {
+    this.mapa.flyTo(
+      { center: item, zoom: 9 }
+    )
+
+  }
+}
 
 
 createMap(){
@@ -68,29 +77,22 @@ createMap(){
     }  
     this.mapa = new mapboxgl.Map({
     container: this.divMapa.nativeElement,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center:this.lngLat,
+    style: 'mapbox://styles/mapbox/light-v10', // Specify which map style to use
+    center:[ -84.1165100,10.0023600],
     zoom:14,
       interactive:this.interactive
     });
 
 
 // Create a default Marker and add it to the map.
-const newMarker = new mapboxgl.Marker({  draggable: true})
-.setLngLat(this.lngLat)
+const newMarker = new mapboxgl.Marker({ color:"#010203",  draggable: false})
+newMarker.setLngLat([ -84.1165100,10.0023600])
+    .setPopup(new mapboxgl.Popup({closeOnClick: false, closeButton: false}).setText("Start Point"))
+    .addTo(this.mapa)
+    .togglePopup()
 .addTo(this.mapa);
+this.getRoute()
 
-
-newMarker.on('dragend', ()=>{
-
-  this.returnstepThree();
-const { lng, lat } = newMarker!.getLngLat();
-this.lngLat  = [lng, lat];
-
-this.createMap();
-
-
-})
 
 this.mapa.addControl(new mapboxgl.NavigationControl());
 //mapa.addControl(new mapboxgl.FullscreenControl());
@@ -116,16 +118,40 @@ this.mapa.addControl(geocoder);
       this.stepTwo = false;
       this.stepThree = true;
       this.lngLat = e.result.center;
+      this.coordinates = [];
+this.coordinates.push(this.lngLat)
+
       this.busquedaMapa(e.result);
 
     
       
     })
+    for (const coordinate of this.coordinates) {
 
+      const newMarker = new mapboxgl.Marker({  color:"#428cff", draggable: true})
+      .setLngLat(this.lngLat)
+      .setPopup(new mapboxgl.Popup({closeOnClick: false, closeButton: false}).setText("End Point"))
+      .addTo(this.mapa)
+      .togglePopup()
+      .addTo(this.mapa);
+      
+      
+      newMarker.on('dragend', ()=>{
+      
+      const { lng, lat } = newMarker!.getLngLat();
+      this.lngLat  = [lng, lat];
+this.coordinates = [];
+this.coordinates.push(this.lngLat)
+      this.createMap();
+      })
+      
+    }
 
     this.mapa.on('load', () => {
       this.mapa.resize();
     });
+
+    this.irMarcador(this.coordinates[this.coordinates.length-1])
 
 }
 busquedaMapa(resultado) {
@@ -146,7 +172,8 @@ this.getCurrentLocation();
 
 
 this.lngLat  = [resp.coords.longitude,resp.coords.latitude];
-
+this.coordinates = [];
+this.coordinates.push(this.lngLat)
 this.createMap();
 
 
@@ -155,5 +182,61 @@ this.createMap();
   err => {
     console.log(err);
   });
+}
+
+async  getRoute() {
+  // make a directions request using cycling profile
+  // an arbitrary start will always be the same
+  // only the end or destination will change
+
+  let firstPart =  `https://api.mapbox.com/directions/v5/mapbox/driving/-84.1165100,10.0023600`
+   let middle = '';
+this.coordinates.forEach(cordinate=>{
+
+  middle += ';'+cordinate
+
+
+})
+  let secondPart = `?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`;
+
+  let final = firstPart + middle +secondPart;
+
+if(this.coordinates.length > 0){
+  const query = await fetch(
+    final,
+    { method: 'GET' }
+  );
+  const json = await query.json();
+
+  console.log(middle)
+  const data = json.routes[0];
+  const route = data.geometry.coordinates;
+  let geojson :any = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: route
+    }
+  };
+  this.mapa.addLayer({
+    id: 'route',
+    type: 'line',
+    source: {
+      type: 'geojson',
+      data: geojson
+    },
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    paint: {
+      'line-color': '#3887be',
+      'line-width': 5,
+      'line-opacity': 0.75
+    }
+})
+}
+
 }
 }
